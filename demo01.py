937db0209b1894f6a84e50440dc7a0e4977a2056
Luyuandan <1464821902@qq.com>
2020-04-18 10:16:31 +0800

    第一次像GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,62 @@
+"""
+print("hello world!") # 字符串
+print(2333) # 整数 
+print(2.333) # 小数
+print(True) # 布尔值
+print(()) # 元组
+print([]) # 数组
+print({}) # 字典
+"""
+# 锄禾日当午
+# 汗滴禾下土
+"""
+print("哈哈",2333)
+
+"""
+# a = float(input("请输入:"))
+# b = float(input("请输入:"))
+# print("input获取的内容:",a+b)
+# a = "哈哈哈"
+# print(len(a))
+
+# 作业
+# a = input("请输入：")
+# b = input("请输入:")
+# print("两段内容的长度：",len(a)+len(b))
+
+
+# # 元组,下标,从0开始编号
+# a = (1,2,3,4,"哈哈","嘻嘻",True,False) 
+
+# # print(a[4])
+# b = (a,"哈哈")
+# print(b[0][3])
+# print(a[0:4])
+
+# a = [1,2,3,4,"哈哈","嘻嘻",True,False]
+# a.append("append")
+# print(a)
+# a.insert(0,"insert")
+# print(a)
+# a.pop(6)
+# print(a)
+
+
+# a = {"name":"zhangsan",0:"哈哈","age":25}
+# # print(a["name"])
+# # a["high"] = "183cm"
+# # print(a)
+# # a["name"] = "李四"
+# # print(a)
+# b = a.get("name")
+# print(b)
+# b = a.pop("name")
+# print(b)
+# print(a)  
+# a.update(name=111111)
+# print(a)
+a = input("请输入姓名:")
+b = input("请输入年龄：")
+c = input("请输入性别：")
+d = {"name":a,"age":b,"sex":c}
+print("个人信息:",d)
